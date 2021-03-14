df6dccc0a47ef229f1ea886af6550d36ff45ae74
ZenryokuService <tak4545@outlook.jp>
2021-03-14 16:54:45 +0900

    H2DBへのSQL実行アプリ作成

--- StudyJavaBasic/src/main/java/ysg/teacher/tkm/app/db/H2dbManager.java
+++ StudyJavaBasic/src/main/java/ysg/teacher/tkm/app/db/H2dbManager.java
@@ -3,6 +3,7 @@ package ysg.teacher.tkm.app.db;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -19,7 +20,8 @@ public class H2dbManager {
 	private Connection con;
 
 	/**
-	 * コンストラクタ、H2DBのコネクションを取得する。
+	 * プライベートコンストラクタ、H2DBのコネクションを取得する。
+	 * {@link H2dbManager#getInstance()}
 	 */
 	private H2dbManager() {
 		try {
@@ -37,6 +39,10 @@ public class H2dbManager {
 		}
 	}
 
+	/**
+	 * このクラスのインスタンスを取得する。
+	 * @return H2dbManagerインスタンス
+	 */
 	public static H2dbManager getInstance() {
 		if (instance == null) {
 			instance = new H2dbManager();
@@ -44,5 +50,62 @@ public class H2dbManager {
 		return instance;
 	}
 
+	/**
+	 * このクラスが解放されるときに起動する。
+	 */
+	@Override
+	public void finalize() {
+		con = null;
+		instance = null;
+	}
 
+	/**
+	 * SELECT文を実行する。
+	 *
+	 * @param sql SELECT文
+	 * @return 成功：
+	 */
+	public boolean executeQuery(String sql) {
+		boolean isSuccess = false;
+		ResultSet result = null;
+		StringBuilder build = new StringBuilder();
+		try {
+			Statement stmt = con.createStatement();
+			result = stmt.executeQuery(sql);
+			ResultSetMetaData meta = result.getMetaData();
+			int count = meta.getColumnCount();
+			for (int i = 1; i <= count; i++) {
+				build.append(meta.getColumnName(i) + ",");
+			}
+			String header = build.toString().substring(0, build.length() -1);
+			System.out.println(header);
+			StringBuilder datas = new StringBuilder();
+			while (result.next()) {
+				for (int i = 1; i <= count; i++) {
+					datas.append(result.getString(i) + ",");
+				}
+				String line = datas.toString().substring(0, datas.length() -1);
+				System.out.println(line);
+			}
+			isSuccess = true;
+		} catch (SQLException se) {
+			se.printStackTrace();
+		}
+		return isSuccess;
+	}
+
+	/**
+	 * Create文などのSELECT以外のSQLを実行する。
+	 * @param sql
+	 */
+	public boolean execute(String sql) {
+		boolean isSuccess = false;
+		try {
+			con.createStatement().execute(sql);
+			isSuccess = true;
+		} catch (SQLException se) {
+			se.printStackTrace();
+		}
+		return isSuccess;
+	}
 }
