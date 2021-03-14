abb7702dbbeae8202b574cf7bc75252a76294130
ZenryokuService <tak4545@outlook.jp>
2021-03-14 16:55:20 +0900

    SQLを実行のコミット漏れ

--- StudyJavaBasic/src/test/java/ysg/teacher/tkm/app/db/H2dbManagerTest.java
+++ StudyJavaBasic/src/test/java/ysg/teacher/tkm/app/db/H2dbManagerTest.java
@@ -1,5 +1,7 @@
 package ysg.teacher.tkm.app.db;
 
+import static org.junit.Assert.*;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -15,4 +17,30 @@ public class H2dbManagerTest {
 	public void test01() {
 		System.out.println("テスト");
 	}
+
+	@Test
+	public void testSql01() {
+		target.executeQuery("select * from test;");
+	}
+
+	@Test
+	public void testCreateTable01() {
+		String sql = "create table if not exists items ("
+				+ "id bigint not null auto_increment, "
+				+ "cetegory_id varchar(20) not null, "
+				+ "item_name varchar(20) not null, "
+				+ "money int not null, "
+				+ "effect_id varchar(20) not null, "
+				+ "effect_value int not null, "
+				+ "weight int not null, "
+				+ "number_of_times int not null);";
+		assertTrue(target.execute(sql));
+	}
+
+	@Test
+	public void testExecuteQuery01() {
+		target.executeQuery("show Tables;");
+	}
+
+
 }
