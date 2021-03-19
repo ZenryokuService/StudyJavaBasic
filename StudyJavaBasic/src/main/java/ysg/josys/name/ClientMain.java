f02a1cd7a44d87d936228dbcd5512ac2906c76d3
余湖 巧 <tkm-yogo@yoshida-g.jp>
2021-03-19 15:13:14 +0900

    NaturalBornServer Ver1.0 入力値を返却する

---
+++ StudyJavaBasic/src/main/java/ysg/josys/name/ClientMain.java
@@ -0,0 +1,115 @@
+package ysg.josys.name;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ * クライアントアプリ、サーバーソケットへデータを送信する。
+ *
+ * @author 作成者の名前
+ * @see NaturalBornServlet
+ */
+public class ClientMain {
+	/** 自身のインスタンス */
+	private static ClientMain main;
+	/** クライアントソケット */
+	private Socket sock = null;
+	/** 改行コード */
+	private static final String SEP = System.lineSeparator();
+
+	public ClientMain() {
+		try {
+			sock = new Socket("localhost", NaturalBornServlet.PORT_NO);
+		} catch (IOException e) {
+			System.out.println("*** ソケットで例外が発生しました。 ***");
+		}
+	}
+
+	/**
+	 * デストラクタ
+	 */
+	@Override
+	public void finalize() {
+		try {
+			sock.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			sock = null;
+		}
+	}
+
+	/**
+	 * 標準入力からサーバーソケットへデータを送信する。
+	 *
+	 * @return 終了時のメッセージ
+	 */
+	public String execute() {
+		try (Scanner scan = new Scanner(System.in)) {
+			while(true) {
+				System.out.println("*** クライアントソケット: 入力 ***");
+				String input = scan.nextLine();
+
+				// リクエストの送信
+				sendRequest(input);
+
+				if ("bye".equals(input)) {
+					System.out.println("*** クライアントアプリを終了します。***");
+					break;
+				}
+				// レスポンスを受信する
+				System.out.println("クライアント: " + getResponse());
+			}
+		}
+
+		return "クライアントを終了しました。";
+	}
+
+	private void sendRequest(String in) {
+		try {
+			System.out.println("*** クライアントソケット: リクエスト送信" + in + " ***");
+			PrintWriter send = new PrintWriter(sock.getOutputStream());
+			send.append(in + SEP);
+			send.flush();
+//			send.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * サーバーからのレスポンスを受ける。
+	 * @throws IOException
+	 */
+	private String getResponse() {
+		StringBuilder build = new StringBuilder();
+		try {
+			System.out.println("*** クライアントソケット: レスポンス受信 ***");
+			BufferedReader response = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+			while (true) {
+				int read = response.read();
+				if (read == 13 || read == 10) {
+					break;
+				}
+				char ch = (char) read;
+				build.append(ch);
+			}
+			//response.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+			build.append(" Errors... ");
+		}
+		System.out.println("*** クライアントソケット: 完了：レスポンス受信 ***");
+		return build.toString();
+	}
+
+	public static void main(String[] args) {
+		ClientMain main = new ClientMain();
+		String mes = main.execute();
+		System.out.println(mes);
+	}
+ }
---
+++ StudyJavaBasic/src/main/java/ysg/josys/name/NaturalBornServlet.java
@@ -0,0 +1,133 @@
+package ysg.josys.name;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * 低レベルSocketサーバー。
+ * サーバーとクライアントの関係を理解するためのサンプルコード。
+ * 単体アプリとする
+ * @author 作成者の名前
+ */
+public class NaturalBornServlet {
+	/** 受け付けるおポート番号 */
+	public static final int PORT_NO = 8081;
+	/** 自分自身のインスタンス */
+	private static NaturalBornServlet main;
+
+	/** リクエスト */
+	private BufferedReader request;
+	/** レスポンス */
+	private PrintWriter response;
+
+	/** サーバーソケット */
+	private ServerSocket server;
+	/** 改行コード */
+	private final String SEP = System.lineSeparator();
+
+	/**
+	 * 外部からの起動を禁止する。プライベートコンストラクタ。
+	 */
+	private NaturalBornServlet() {
+		try {
+			server = new ServerSocket(PORT_NO);
+		} catch (IOException e) {
+			System.out.println("*** サーバーソケットの起動に失敗しました。 ***");
+			e.printStackTrace();
+			System.exit(-1);
+		}
+	}
+
+	/**
+	 * デストラクタ。
+	 * サーバーソケットのクローズ、メモリの開放を行う。
+	 */
+	public void finalize() {
+		try {
+			server.close();
+		} catch (IOException e) {
+			System.out.println("*** サーバーソケットの終了に失敗しました。 ***");
+			e.printStackTrace();
+		} finally {
+			server = null;
+			main = null;
+			System.exit(-1);
+		}
+	}
+
+	/**
+	 * 起動した、サーバーソケットで待機を行う。
+	 */
+	private void execute() throws IOException {
+		System.out.println("*** サーバーソケット起動 ***");
+		System.out.println("*** サーバーソケット: accept() ***");
+		Socket sock = server.accept();
+		while(true) {
+			System.out.println("*** サーバー情報: " + sock.getPort() + " ***");
+			// リクエスト
+			request = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+			// レスポンス
+			response = new PrintWriter(new OutputStreamWriter(sock.getOutputStream()));
+
+			// リクエストの読み込み
+			String inputTxt = readRequest(request);
+			if (inputTxt != null && inputTxt.startsWith("bye")) {
+				System.out.println("*** サーバーを終了します。 ***");
+				break;
+			}
+
+			// レスポンスを返す
+			response.append(inputTxt + SEP);
+			response.flush();
+			System.out.println("*** サーバーソケット: レスポンス送信 " + inputTxt + "***");
+
+//			request = null;
+//			response = null;
+		}
+	}
+
+	/**
+	 * 受信した、メッセージを読み込む。
+	 *
+	 * @param in Socketより取得した入力ストリーム
+	 * @return
+	 * @throws IOException
+	 */
+	private String readRequest(BufferedReader in) throws IOException {
+		System.out.println("*** サーバーソケット: readRequest() ***");
+		int read = 0;
+		StringBuilder inputTxt = new StringBuilder();
+        // CRとCRLFの場合で入力が終了している時がある
+        while((read = in.read()) != -1) {
+            // 空の場合
+            if (read == 10 || read == 13) {
+            	break;
+            }
+            char ch = (char) read;
+            inputTxt.append(ch);
+        }
+		System.out.println("*** サーバーソケット: 完了：readRequest() ***");
+        return inputTxt.toString();
+	}
+
+	/**
+	 * メインメソッド、ここから始まる。
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		main = new NaturalBornServlet();
+
+		try {
+			main.execute();
+		} catch (IOException e) {
+			System.out.println("*** 例外が発生しました。 " + e.getMessage() + "***");
+			e.printStackTrace();
+		}
+
+	}
+}
